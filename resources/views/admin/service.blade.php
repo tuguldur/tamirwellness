cacf24b0757b1e3f2a369d7e3789339b525a6398
tuguldur <aa402115@gmail.com>
2019-06-20 19:03:23 +0800

    HOME

--- resources/views/admin/service.blade.php
+++ resources/views/admin/service.blade.php
@@ -8,10 +8,20 @@
                 <div class="card-header">
                     <h4 class="card-title">Add Service</h4>
                  </div>
-                </div>
                 <div class="card-body">
-                  
-                </div>
+                  <form action="/admin/service" method="POST">
+                        <div class="form-group">
+                          <label for="image_description">Description</label>
+                          <input type="text" class="form-control" id="image_description" placeholder="Image Description" name="desc" required>
+                        </div>
+                        <div class="custom-file">
+                              <input type="file" class="custom-file-input" id="input-file" name="file">
+                              <label class="custom-file-label" for="input-file">Choose Image</label>
+                            </div>
+                        <button type="submit" class="btn btn-primary" disabled="" id="contact_submit">Save</button>
+                        <input type="hidden" name="_token" value="kOIKpVfmWR8KL1geQg0oD3AVuOrLeIMBtchcKd0S">                    
+                  </form>
+               
               </div>
             </div>
           </div>
